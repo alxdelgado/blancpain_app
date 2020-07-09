0037b921d1b4a978e7d76c572ee46cd6e4ac3145
Alex Delgado <delgadoalex@icloud.com>
2020-07-08 22:53:55 -0500

    added tslint config file, app is up and running

---
+++ src/containers/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function App() {
+    return (
+        <div className='container'>
+            <div>
+                <h1>Blancpain</h1> 
+            </div>
+        </div>
+    );
+}
